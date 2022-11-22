9846abbc1b25c9de0cfcd59d981ded9c94daeb3b
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-11-22 13:37:06 +0100

    fix import errors

--- packages/dapp/src/index.tsx
+++ packages/dapp/src/index.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
