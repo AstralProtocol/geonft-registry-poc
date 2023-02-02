299b2bc425a5adb71a6d975b8c9b87bce10cc3e0
Daniel Serrano <danielserrano.contacto@gmail.com>
2023-02-02 15:54:35 +0100

    fix vite issues

--- packages/dapp-vite/vite.config.ts
+++ packages/dapp-vite/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import GlobalPolyFill from "vite-plugin-global-polyfill";
-// import GlobalPolyFill from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +10,9 @@ export default defineConfig({
       protocolImports: true,
     }),
   ],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
   define: {
     "process.env": {},
     global: {},
