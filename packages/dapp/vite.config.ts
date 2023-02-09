7277857de84d2268384a1daf04bad51ada36273e
Daniel Serrano <danielserrano.contacto@gmail.com>
2023-02-09 11:41:05 +0100

    add favicon

--- packages/dapp/vite.config.ts
+++ packages/dapp/vite.config.ts
@@ -4,6 +4,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     react(),
     nodePolyfills({
@@ -18,8 +19,5 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-  },
-  preview: {
-    port: 8080,
   },
 });
