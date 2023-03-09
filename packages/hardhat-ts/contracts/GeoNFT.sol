52daa2cb1d31abce5ca96142e64be2e0e387ba4b
Daniel Serrano <danielserrano.contacto@gmail.com>
2023-03-09 19:22:35 +0100

    fix minor issues

--- packages/hardhat-ts/contracts/GeoNFT.sol
+++ packages/hardhat-ts/contracts/GeoNFT.sol
@@ -113,9 +113,10 @@ contract GeoNFT is
     function _beforeTokenTransfer(
         address from,
         address to,
-        uint256 tokenId
+        uint256 tokenId,
+        uint256 batchSize
     ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     function _burn(
